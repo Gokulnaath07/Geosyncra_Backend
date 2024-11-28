0e6dfb9f16ad70279f424a9f2e876209d29479e6
Gokulnaath07 <gokuhallows003@gmail.com>
2024-11-27 19:04:47 -0500

    This is the updated version of backend.

--- src/main/java/com/teamseven07/geosyncra_backend/controller/ImageController.java
+++ src/main/java/com/teamseven07/geosyncra_backend/controller/ImageController.java
@@ -1,16 +1,18 @@
 package com.teamseven07.geosyncra_backend.controller;
 
+import com.teamseven07.geosyncra_backend.Repository.ImageChildRepository;
 import com.teamseven07.geosyncra_backend.Repository.ImageRepository;
 import com.teamseven07.geosyncra_backend.Service.ImageService;
-import com.teamseven07.geosyncra_backend.model.ImageDto;
-import com.teamseven07.geosyncra_backend.model.ImageEntity;
+import com.teamseven07.geosyncra_backend.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,14 +24,38 @@ public class ImageController {
     private ImageService imageService;
     @Autowired
     private ImageRepository imageRepository;
-
+    @Autowired
+    private ImageChildRepository imageChildRepository;
 
 
     @GetMapping("/images")
-    public ResponseEntity<List<ImageEntity>> getAllImages() {
-        return new ResponseEntity<>(imageService.getAllImages(), HttpStatus.OK);
+    public ResponseEntity<Page<ImageAllDto>> getAllImages(@RequestParam(defaultValue = "0") int page,
+                                                          @RequestParam(defaultValue = "3") int size) {
+        return new ResponseEntity<>(imageService.findAllDtoPage(page, size), HttpStatus.OK);
     }
 
+
+    @GetMapping("/images/{imageId}/data")
+    public ResponseEntity<byte[]> getImageDataById(@PathVariable Integer imageId) {
+        Optional<ImageChildEntity> imageChildEntityOptional = imageChildRepository.findById(imageId);
+
+        if (imageChildEntityOptional.isPresent()) {
+            ImageChildEntity imageChildEntity = imageChildEntityOptional.get();
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(imageChildEntity.getImageType()))
+                    .body(imageChildEntity.getImageData());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+
+    @GetMapping("/{parentId}/child")
+    public ResponseEntity<List<ImagePictureDto>> getChildImagesByParentId(@PathVariable Integer parentId) {
+        List<ImagePictureDto> childImages = imageService.getAllChildImagesByParentId(parentId);
+        return ResponseEntity.ok(childImages);
+    }
 
 
     @PostMapping("/uploadImages")
@@ -46,44 +72,70 @@ public class ImageController {
         }
     }
 
-    @GetMapping(value = "/images/{imageid}/image")
-    public ResponseEntity<byte[]> getImageByProductId(@PathVariable("imageid") Integer imageId) {
-        // Retrieve the ImageEntity from the database by ID
-        Optional<ImageEntity> imageOptional = imageRepository.findById(imageId);
 
-        if (imageOptional.isPresent()) {
-            ImageEntity imageEntity = imageOptional.get();
-
-            // Check that both imageType and imageData are not null
-            if (imageEntity.getImageType() != null && imageEntity.getImageData() != null) {
-                return ResponseEntity.ok()
-                        .contentType(MediaType.parseMediaType(imageEntity.getImageType()))
-                        .body(imageEntity.getImageData());
-            } else {
-                return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).build();
-            }
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    @PostMapping("/{imageId}/addChildImage")
+    public ResponseEntity<?> addChildImage(@PathVariable Integer imageId,
+                                           @RequestParam("imageName") String imageName,
+                                           @RequestParam("imageFile") MultipartFile imageFile) {
+        try {
+            ImageChildEntity savedChildImage = imageService.addChildImage(imageId, imageName, imageFile);
+            return new ResponseEntity<>(savedChildImage, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
 
     @GetMapping("/location/{location}")
-    public ResponseEntity<List<ImageDto>> findByLocation(
+    public ResponseEntity<Page<ImageDto>> findByLocation(
             @PathVariable("location") String location,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "3") int size) {
 
-        List<ImageDto> imageResponse = imageService.findByLocation(location, page, size);
+        Page<ImageDto> imageResponse = imageService.findByLocation(location, page, size);
         return new ResponseEntity<>(imageResponse, HttpStatus.OK);
     }
 
 
     @GetMapping("images/{id}/geoLocation")
-    public ResponseEntity<String> getGeoLocation(@PathVariable Integer id) {
+    public ResponseEntity<String> getGeoLocation(@PathVariable Integer id) throws IOException {
+        // Call the service method to fetch the geoLocation by id
         String geoLocation = imageService.getGeoLocation(id);
+
+        // Return the geoLocation in the response with HTTP status OK (200)
         return new ResponseEntity<>(geoLocation, HttpStatus.OK);
     }
+    @PostMapping("/{id}/comments")
+    public ResponseEntity<?> addComment(@PathVariable Integer id, @RequestParam("comments") String comment) {
+        try{
+            ImageCommentChildEntity savedComment = imageService.addComment(id, comment);
+            return new ResponseEntity<>(savedComment, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
+    @PutMapping("/{id}/like")
+    ResponseEntity<?>addLikes(@PathVariable Integer id,
+                              @RequestParam("like") Integer like){
+        try{
+            ImageLikesEntity savedLikes=imageService.addLikes(id, like);
+            return new ResponseEntity<>(savedLikes, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("images/{id}/comment")
+    ResponseEntity<List<ImageCommentDto>> getComments(@PathVariable Integer id){
+        List<ImageCommentDto> showComments=imageService.showComments(id);
+        return new ResponseEntity<>(showComments,HttpStatus.OK);
+    }
+
+    @GetMapping("images/{id}/likes")
+    ResponseEntity<List<ImageLikesDto>> viewLikes(@PathVariable Integer id){
+        List<ImageLikesDto> viewLikes=imageService.viewLikes(id);
+        return new ResponseEntity<>(viewLikes,HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/teamseven07/geosyncra_backend/model/ImageAllDto.java
+++ src/main/java/com/teamseven07/geosyncra_backend/model/ImageAllDto.java
@@ -1,21 +1,30 @@
 package com.teamseven07.geosyncra_backend.model;
 
-public class ImageDto {
+public class ImageAllDto {
 
     private Integer id;
     private String name;
     private String location;
     private String description;
+    private String geoLocation;
 //    @Lob
 //    private byte[] imageData;
 
-    public ImageDto(Integer id,String name, String location, String description) {
+    public ImageAllDto(Integer id,String name, String location, String description, String geoLocation) {
         this.id = id;
         this.name = name;
         this.location = location;
         this.description = description;
+        this.geoLocation = geoLocation;
+//        this.imageData = imageData;
     }
+//    public byte[] getImageData() {
+//        return imageData;
+//    }
 
+//    public Integer getId() {
+//        return id;
+//    }
 
     public Integer getId() {
         return id;
@@ -30,5 +39,9 @@ public class ImageDto {
 
     public String getDescription() {
         return description;
+    }
+
+    public String getGeoLocation() {
+        return geoLocation;
     }
 }
